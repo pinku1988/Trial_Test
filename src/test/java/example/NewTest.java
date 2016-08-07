9a0ce27a5f759e59fc4aa7a26799fa394685acf0
pinku1988 <soyam.pinku@gmail.com>
2016-08-08 00:53:27 +0530

    test

---
+++ src/test/java/example/NewTest.java
@@ -0,0 +1,22 @@
+package example;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class NewTest {
+  @Test
+  public void f() {
+	  System.out.println("A");
+  }
+  @BeforeTest
+  public void beforeTest() {
+	  System.out.println("B");
+  }
+
+  @AfterTest
+  public void afterTest() {
+	  System.out.println("C");
+  }
+
+}
